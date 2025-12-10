bcac643069031b21292b7ab4b00ce9412b6e5f1c
SebastianAyala13 <sebasayala0113@gmail.com>
2025-12-10 16:47:26 -0500

    actualizacion de zipcodes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,6 +113,23 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End TrustedForm Script */}
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1538478924006867');
+            fbq('track', 'PageView');`,
+          }}
+        />
+        {/* End Meta Pixel Code */}
         
         {/* Removed alternative manual TF implementation to match Premium-bathrooms pattern */}
         
@@ -135,9 +152,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </noscript>
         {/* TrustedForm pixel tracking (noscript) */}
-        <noscript>
-          <img src="https://api.trustedform.com/ns.gif" />
-        </noscript>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img src="https://api.trustedform.com/ns.gif" alt="" style="display:none" />',
+          }}
+        />
+        {/* Meta Pixel (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none" alt="" src="https://www.facebook.com/tr?id=1538478924006867&ev=PageView&noscript=1" />',
+          }}
+        />
         {/* End Google Tag Manager (noscript) */}
         
         {/* 🔁 Background Video Global */}
